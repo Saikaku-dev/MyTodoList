a1cd05e87030fadc73b09d6068f6143eddd7b82f
Saikaku <grnmnsta@gmail.com>
2025-06-13 18:07:31 +0900

    ローカルに保存ためのImplを実装

--- MyTodoList/Data/LocalTaskRepositoryImpl.swift
+++ MyTodoList/Data/LocalTaskRepositoryImpl.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+// SwiftDataでLocalに保存
+class LocalTaskRepositoryImpl: TaskRepository {
+    private var localTasks:[Task] = []
+    
+    func fetchAllTasks() -> [Task] {
+        localTasks
+    }
+    
+    func saveTask(_ task: Task) {
+        localTasks.append(task)
+    }
+    
+    func deleteTask(_ task: Task) {
+        localTasks.removeAll(where: { $0.id == task.id })
+    }
+    
+    func updateTask(_ task: Task) {
+        if let index = localTasks.firstIndex(where: { $0.id == task.id }) {
+            localTasks[index] = task
+        }
+    }
+}
