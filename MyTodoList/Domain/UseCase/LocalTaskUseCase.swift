dc2d1a3b6ad572e87fc4a332bf0e6e0f5a62637a
Saikaku <grnmnsta@gmail.com>
2025-06-13 18:08:21 +0900

    読み込み・追加・更新のメソッドを追加

--- MyTodoList/Domain/UseCase/LocalTaskUseCase.swift
+++ MyTodoList/Domain/UseCase/LocalTaskUseCase.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class LocalTaskUseCase {
+    private let repository: TaskRepository
+    
+    init(repository: TaskRepository) {
+        self.repository = repository
+    }
+    
+    func fetchAllTasks() -> [Task] {
+        repository.fetchAllTasks()
+    }
+    
+    func addTask(_ task: Task) {
+        repository.saveTask(task)
+    }
+    
+    func updateTask(_ task: Task) {
+        repository.updateTask(task)
+    }
+}
