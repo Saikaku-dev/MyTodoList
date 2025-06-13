6c1937da3e7b1f20afbf92db6208e570d019c7f6
Saikaku <grnmnsta@gmail.com>
2025-06-13 18:10:30 +0900

    Taskに対してのロード・追加・状態変更のロジックを作成

--- MyTodoList/Presentation/CurrentTask/CurrentTaskViewModel.swift
+++ MyTodoList/Presentation/CurrentTask/CurrentTaskViewModel.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import Observation
+
+@Observable
+class CurrentTaskViewModel {
+    var tasks:[Task] = []
+    var taskTitle: String = ""
+    var showAddTaskSheet: Bool = false
+    private let localTaskUseCase: LocalTaskUseCase
+    
+    init(localTaskUseCase: LocalTaskUseCase) {
+        self.localTaskUseCase = localTaskUseCase
+        loadLocalTasks()
+    }
+    
+    func closeSheet() {
+        showAddTaskSheet = false
+    }
+    
+    func loadLocalTasks() {
+        tasks = localTaskUseCase.fetchAllTasks()
+    }
+    
+    func addTask(title: String) {
+        guard !taskTitle.isEmpty else { return }
+        let newTask = Task(title: title)
+        localTaskUseCase.addTask(newTask)
+        loadLocalTasks()
+        taskTitle = ""
+    }
+    
+    func toggleCompeted(for task: Task) {
+        var updateTask = task
+        updateTask.toggleCompleted()
+        localTaskUseCase.updateTask(updateTask)
+        loadLocalTasks()
+    }
+}
