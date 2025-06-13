c28125468ab6b0647aab10058c96c17285167d3a
Saikaku <grnmnsta@gmail.com>
2025-06-13 18:06:30 +0900

    TaskRepositoryを作成

--- MyTodoList/Domain/Repository/TaskRepository.swift
+++ MyTodoList/Domain/Repository/TaskRepository.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+//Taskに対して、何をする(抽象的)
+protocol TaskRepository {
+    func fetchAllTasks() -> [Task]
+    func saveTask(_ task: Task)
+    func deleteTask(_ task: Task)
+    func updateTask(_ task: Task)
+}
