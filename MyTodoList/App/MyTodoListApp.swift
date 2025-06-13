492cb2ed19f78cebdc854abd49d293ecd90dac47
Saikaku <grnmnsta@gmail.com>
2025-06-13 14:27:48 +0900

    エントリーポイントのViewをLoginViewに変更

---
+++ MyTodoList/App/MyTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoListApp.swift
+//  MyTodoList
+//
+//  Created by cmStudent on 2025/06/13.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
