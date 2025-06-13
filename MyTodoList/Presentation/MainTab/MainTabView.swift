cac69a8fbddaa33d8b08eb88b4eb264dd2998b46
Saikaku <grnmnsta@gmail.com>
2025-06-13 14:45:29 +0900

    画面遷移機能の追加

---
+++ MyTodoList/Presentation/MainTab/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  MyTodoList
+//
+//  Created by cmStudent on 2025/06/13.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
