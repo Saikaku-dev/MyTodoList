02910c41753018f20ed88afdc37ba442cc6ece1c
Saikaku <grnmnsta@gmail.com>
2025-06-16 13:43:28 +0900

    Feildに排列のための作成時間と完成時間を追加

--- MyTodoList/Domain/Entity/Task.swift
+++ MyTodoList/Domain/Entity/Task.swift
@@ -10,17 +10,26 @@ import SwiftData
 
 @Model
 class Task:Identifiable {
-    @Attribute(.unique) var id: String
+    @Attribute(.unique) let id: String
     var title: String
     var isCompleted: Bool
+    var createdDate: Date
+    var completedDate: Date?
     
-    init(id: String = UUID().uuidString, title: String, isCompleted: Bool = false) {
+    init(id: String = UUID().uuidString,
+         title: String,
+         isCompleted: Bool = false,
+         createdDate: Date = Date(),
+         completedDate: Date? = nil) {
         self.id = id
         self.title = title
         self.isCompleted = isCompleted
+        self.createdDate = createdDate
+        self.completedDate = completedDate
     }
     
     func toggleCompleted() {
         isCompleted.toggle()
+        completedDate = isCompleted ? Date() : nil
     }
 }
